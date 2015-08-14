d5db9e2d0196f24156f6aa12defb5bf7decd6c3b
Matt Turnure <matt@mattturnure.com>
2015-08-14 13:49:30 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Crossfit PR Weight Calculator
+
+Tired of keeping track of your 60%, 70%, 90% max? Just plug in your PR and let the calculator do it for you.
+
+<code>npm install</code>
+<code>bower install</code>
---
+++ src/assets/js/main.js
@@ -0,0 +1,53 @@
+(function (doc) {
+    'use strict';
+
+    var prForm  = doc.getElementById('pr-form'),
+        prInput = doc.getElementById('pr'),
+        results = doc.getElementById('results'),
+        html    = doc.createDocumentFragment();
+
+    function getPR(e) {
+        var list = document.createElement('ul');
+        e.preventDefault();
+
+        if ( isNumber(+prInput.value) ) {
+            makeList();
+        } else {
+            console.log('You must enter a number');
+            prInput.select();
+        }
+    }
+
+    function isNumber(num) {
+        return typeof num === 'number' && !isNaN(num);
+    }
+
+    function makeList() {
+        var percent = 100,
+            li      = doc.createElement('li'),
+            item;
+
+        li.className = 'list-group-item';
+        results.innerHTML = '';
+
+        while (percent--) {
+            if (percent % 10 === 0 && percent !== 0) {
+                item = li.cloneNode(true);
+                item.innerHTML = makePercentageHTML(percent, +prInput.value);
+                html.appendChild(item);
+            }
+        }
+
+        results.appendChild(html);
+    }
+
+    function makePercentageHTML(percent, val) {
+        return '<span class="percent-value text-primary">' + convert(percent, val).toFixed() + '</span><span class="percent-label pull-right text-muted">' + percent + '%</span>';
+    }
+
+    function convert(percentage, val) {
+        return (val * percentage * 1000000) / 100000000;
+    }
+
+    prForm.addEventListener('submit', getPR, false);
+}(document));
